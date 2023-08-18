3b95dc5f69f69c34a8a21bd51e3150e422e0629f
JulianaSoba <julianasobajava23@gmail.com>
2023-08-18 17:04:47 +0100

    pushing changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,16 @@
-import {Home as HomeIcon, Search,Library} from 'lucide-react';
+import Aside from "@/components/aside";
+
 
 export default function Home() {
   return (
-    <div
-      className='h-screen flex flex-col'
-    >
-     <div className="flex flex-1">
-      <aside className="w-72 bg-zinc-950">
-        <nav className="flex flex-col">
-          <a href=""><HomeIcon/> Home</a>
-          <a href=""><Search/> Search</a>
-          <a href=""><Library/> Your Library</a>
-        </nav>
-      </aside>
-      <main className="flex-1 p-4">main content</main>
-     </div>
-     <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+       <Aside/>
+        <main className="flex-1 p-6">main content</main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
     </div>
-  )
+  );
 }
